5d1b1c34c2452f123a315de26d281b463b5712dc
Ahmed Raed <ahmed.raed@vultara.com>
2025-06-14 18:08:43 +0300

    Auto commit on Sat 14-06(Jun)-25 06-08-43 PM

--- git_bash.sh
+++ git_bash.sh
@@ -9,7 +9,7 @@ gitoutput=$(git status --porcelain)
 echo -e "$gitoutput"
 
 if [[ -n $gitoutput ]]; then
-    commitmessage=${1:- "Auto commit on $(date +'%a %d-%m(%b)-%Y %I-%M-%S %p')"}
+    commitmessage=${1:- "Auto commit on $(date +'%a %d-%m(%b)-%y %I-%M-%S %p')"}
     echo $commitmessage
 
     git add .
