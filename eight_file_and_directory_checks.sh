b40704b1865197174314b53bf8d3fea63878bd25
Ahmed Raed <ahmed.raed@vultara.com>
2025-06-14 18:06:29 +0300

    Auto commit on Saturday 14-06(Jun)-2025 06-06-29 PM

---
+++ eight_file_and_directory_checks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -f "one.sh" ]; then
+    echo "First script exits"
+fi
+
+
+if [ -d "/etc" ]; then
+    echo "/etc exists"
+fi
+
+
---
+++ git_bash.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Start git bash"
+
+cd "$(dirname "$0")"
+
+gitoutput=$(git status --porcelain)
+
+echo -e "$gitoutput"
+
+if [[ -n $gitoutput ]]; then
+    commitmessage=${1:- "Auto commit on $(date +'%A %d-%m(%b)-%Y %I-%M-%S %p')"}
+    echo $commitmessage
+
+    git add .
+    git commit -m "$commitmessage"
+    git push
+
+fi
+echo "End git bash"
